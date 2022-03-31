9c613484b8ee339eeaccb3a2f44b0e53439ec533
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-31 07:58:48 +0300

    configure tooling for typescript

--- auth/index.ts
+++ auth/index.ts
@@ -1,9 +1,14 @@
+import { HttpError } from '@adwesh/common';
 import mongoose from 'mongoose';
 
 import { app } from './app';
 
+if (!process.env.MONGO_URI) {
+  throw new HttpError('MONGO_URI must be defined', 500);
+}
+
 const startUp = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI!);
   app.listen(5000);
   console.log('Connected to Auth Service, Listening on port: 5000');
 };
